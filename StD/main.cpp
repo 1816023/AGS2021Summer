80205671d818bbd3edcc981d4e7c09c26e16bab0
kimura <ihgakjiguaebgo;ahgikj@jp>
2021-04-14 12:29:47 +0900

    ひな形&ignore更新

--- StD/main.cpp
+++ StD/main.cpp
@@ -1,1 +1,20 @@
 #include <DxLib.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+
+	ChangeWindowMode(true);
+	SetMainWindowText(L"DC&IC");
+	SetGraphMode(1024, 640, 16);
+	if (DxLib_Init() == -1)
+	{
+		return -1;
+	}
+
+	while (CheckHitKey(KEY_INPUT_ESCAPE) == 0 && ProcessMessage() == 0)
+	{
+		
+	}
+	DxLib_End();
+	return 0;
+}
