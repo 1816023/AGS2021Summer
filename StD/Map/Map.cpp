0eb0aef5a7db299471b2f645116841f40dc0f2ce
J-Fox <yuy01227182@i.softbank.jp>
2021-04-15 15:35:44 +0900

    mapdateの追加

--- StD/Map/Map.cpp
+++ StD/Map/Map.cpp
@@ -1,1 +1,36 @@
+#include <DxLib.h>
 #include "Map.h"
+
+Map::Map()
+{
+}
+
+Map::~Map()
+{
+}
+
+int Map::Update()
+{
+	return 0;
+}
+
+void Map::Draw()
+{
+
+
+}
+
+bool Map::SetUp(std::string mapName)
+{
+	Map::LoadMap(mapName);
+	tinyxml2::XMLElement* elm = document.FirstChildElement("map");
+	mapSize_.y = elm->IntAttribute("hight");
+	mapSize_.x = elm->IntAttribute("width");
+	return true;
+}
+
+void Map::LoadMap(std::string mapName)
+{
+	auto path = "./data/mapData/" + mapName+".xml";
+	document.LoadFile(path.c_str());
+}
--- StD/Map/Map.h
+++ StD/Map/Map.h
@@ -1,5 +1,34 @@
 #pragma once
+#include "../VECTOR2.h"
+#include <string>
+#include <vector>
+#include "../tinyxml2/tinyxml2.h"
+
+enum class MapChipName:char
+{
+	MAINSTAY,		//防衛拠点
+	SPAWNER,		//敵出現位置
+	ROOT,			//敵侵攻ルート
+	FIELD,			//自機配置可能箇所
+	WALL,			//自機配置不可能箇所
+	MAX
+
+};
+using dataVec = std::vector<std::vector<MapChipName>> ;
 class Map
 {
+public:
+	Map();
+	virtual ~Map();
+	virtual int Update();
+	virtual void Draw();
+	virtual bool SetUp(std::string mapName);		// マップ情報の初期化、マップデータの名前(拡張子なし)を指定
+protected:
+	void LoadMap(std::string mapName);				// マップをロードする、マップデータの名前(拡張子なし)を指定
+	VECTOR2 mapSize_;								// マップのchip数
+	VECTOR2 chipSize_;								// 1chipのサイズ
+	dataVec mapData_;								// マップデータの格納2次元配列
+	tinyxml2::XMLDocument document;
 };
+
 
