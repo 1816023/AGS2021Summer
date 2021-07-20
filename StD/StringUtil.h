e8413ec380902259c513895ab4ef9776fae0802e
kimura <ihgakjiguaebgo;ahgikj@jp>
2021-07-20 15:41:25 +0900

    フォント記録用関数追加

--- StD/StringUtil.h
+++ StD/StringUtil.h
@@ -3,7 +3,6 @@
 #include <map>
 #define _WtS(x) StringUtil::WStringToString(x)
 #define _StW(x) StringUtil::StringToWString(x)
-
 struct StringUtil
 {
 public:
@@ -13,11 +12,11 @@ public:
 	static std::wstring StringToWString(std::string oString);
 	// 拡張子削除
 	static std::string SpritExtention(std::string string);
-	//// フォント追加
-	//static void AddStrFont(int fontSize, std::wstring name, int thick = -1);
-	//// フォント取得
-	//static int GetStrFont(std::wstring name);
-
-	//static std::map<std::wstring, int>strFonts_;
+	// フォント追加
+	static void AddStrFont(int fontSize, std::wstring name, int thick = -1);
+	// フォント取得
+	static int GetStrFont(std::wstring name);
+	private:
+	static std::map<std::wstring, int>strFonts_;
 };
 
