93dfee6284b54a64af36c0e32383ec103d7bbf41
J-Fox <yuy01227182@i.softbank.jp>
2021-08-06 16:34:06 +0900

    erorr修正

--- StD/CustomState/RootCustom.h
+++ StD/CustomState/RootCustom.h
@@ -139,14 +139,14 @@ struct RootCustom :
 private:
 	bool Save(CustomMapScene* scene)
 	{
-		auto doc = scene->cusMap_->GetDocument();
+		/*auto doc = scene->cusMap_->GetDocument();
 		auto* rootElm = doc.FirstChildElement("root");
 		for (int a = 0; a < rootMap_.size(); a++)
 		{
 			auto* newElm = rootElm->InsertNewChildElement("root");
 			rootElm->SetAttribute("id", 1);
 
-		}
+		}*/
 		return true;
 	}
 	bool Next(CustomMapScene* scene)
